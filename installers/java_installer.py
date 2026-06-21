7b74252295efbe609abca05bb6851e16dc7fa287
jonterry <jterry8468@hotmail.com>
2026-06-21 14:06:39 -0400

    Created java_installer.py

---
+++ installers/java_installer.py
@@ -0,0 +1,41 @@
+import subprocess
+
+def is_java_installed():
+    try:
+        subprocess.run(
+            ["java", "-version"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        return True
+    except Exception:
+        return False
+
+
+def install_java():
+    if (is_java_installed()):
+        print("✓ Java is already installed")
+        return
+
+    print("Installing Java...")
+
+    try:
+        subprocess.run(
+            [
+                "winget",
+                "install",
+                "-e",
+                "--id",
+                "Microsoft.OpenJDK.21"
+
+            ],
+            check=True
+        )
+
+        print("✓ Java installed successfully")
+
+    except subprocess.CalledProcessError:
+        print("✗ Failed to install Java")
+
+
